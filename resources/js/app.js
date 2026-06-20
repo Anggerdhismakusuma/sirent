2c62b69a1627b72e696282ca856f22d0a3a58c35
Kent Nathanael <100405144+KentNathnael@users.noreply.github.com>
2026-06-21 01:26:13 +0700

    feat: about-us page finished

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../scss/app.scss'
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,45 @@
 @section('title', 'About Us')
+@vite(['resources/js/app.js'])
 
-<div name="bg-img">
-    <img src="{{ asset('images/bg-about.png') }}" alt="" class="mx-0 my-0">
-</div>
-<div name="bg-colour">
+<section class="about-page text-white">
+    <a class="about-back" href="{{ url()->previous() }}"> {{-- route untuk back button blm diisi menunggu push angger --}}
+        <i class="bi bi-chevron-left"></i>
+    </a>
 
-</div>
+    <div class="container-fluid p-0">
+        <div class="row g-0 min-vh-100">
+            <div class="col-lg-7 about-left d-flex align-items-center justify-content-center">
+                <div class="about-content">
+                    <h1>SI-RENT</h1>
+                    <p>
+                        We are multi-vendor website that aims to provide hobby and
+                        equipment rental services, such as cameras, drones and other
+                        tools, while maintaining and guaranteeing security between
+                        renters and owners of goods.
+                    </p>
+
+                    <div class="about-features">
+                        <div class="about-feature-item">
+                            <h3>Variety</h3>
+                            <p>Find all kinds of hobby gear in one place.</p>
+                        </div>
+    
+                        <div class="about-feature-item">
+                            <h3>Affordable</h3>
+                            <p>Rent high quality equipment without overspending.</p>
+                        </div>
+    
+                        <div class="about-feature-item">
+                            <h3>Trusted</h3>
+                            <p>Secure transactions between renters and owners.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-5 about-right d-flex align-items-center justify-content-center">
+                <img src="{{ asset('images/logo-sirent 1.png') }}" alt="SI-RENT Logo" class="about-logo">
+            </div>
+        </div>
+    </div>
+</section>
